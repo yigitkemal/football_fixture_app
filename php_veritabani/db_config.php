41c0d65472b3ecfd25a639352c0031a199bc7c20
yigitkemal <40325505+yigitkemal@users.noreply.github.com>
2021-05-08 15:07:58 +0300

    All teams were purchased by providing database connection with Retrofit.

--- php_veritabani/db_config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('DB_USER', 'u561600220_yigitkemal');
-define('DB_PASSWORD','%Naber5Football');
-define('DB_DATABASE', 'u561600220_football_api');
-define('DB_SERVER', 'mysql.hostinger.web.tr');
-?>
