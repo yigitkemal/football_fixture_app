62ce441322e62f2fc7e52edededfcb008535c5c1
yigitkemal <40325505+yigitkemal@users.noreply.github.com>
2021-05-11 15:01:10 +0300

    Php API
    Written api with php.

---
+++ php_api/db_config.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_USER', 'u561600220_yigitkemal');
+define('DB_PASSWORD','%Naber5Football');
+define('DB_DATABASE', 'u561600220_football_api');
+define('DB_SERVER', 'mysql.hostinger.web.tr');
+?>
